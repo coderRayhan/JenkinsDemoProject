4feaed27f62d7e4232375f023e745a7281779bb5
Md. Rayhanur Rashid <rakibrayhan8@gmail.com>
2023-11-13 22:49:55 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.WebHost.UserUrls("http://172.29.30.158:4040");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
